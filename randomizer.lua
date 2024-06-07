1ebf61c7485da992af3f8c819d5d72308fce5f8c
BurndiL <94790464+BurndiL@users.noreply.github.com>
2024-06-07 13:35:17 +0200

    Update randomizer.lua
    Fixed some indentation and formatting
    
    Added loading of new profile for randomizer

--- randomizer.lua
+++ randomizer.lua
@@ -3,9 +3,19 @@
 --- MOD_ID: Rando
 --- MOD_AUTHOR: [Burndi, Silvris]
 --- MOD_DESCRIPTION: Archipelago
-
 ----------------------------------------------
 ------------MOD CODE -------------------------
+-- TODO
+-- TECH:
+-- make most functions only execute when balatro profile is loaded 
+-- load Balatro profile 
+-- parse AP messages 
+-- map ids to checks
+-- lock/unlock decks depending on AP 
+-- FEATURES:
+-- 
+-- Hint Pack
+
 
 this_mod = SMODS.current_mod
 
@@ -18,15 +28,18 @@ local isInProfileTabCreation = false
 local isInProfileOptionCreation = false
 local profileAP_Id = -1
 
+function isAPProfileLoaded()
+    return G.focused_profile == profileAP_Id
+end
+
 G.FUNCS.APConnect = function()
     local APInfo = json.encode(G.AP)
     save_file('APSettings.json', APInfo)
 
-
     -- set profile name to slot name (not sure if this is smart, also doesnt work yet)
     G.PROFILES[profileAP_Id]['name'] = G.AP['APSlot']
-    G:save_settings()
-    G.FILE_HANDLER.force = true
+    G.FUNCS.load_profile(false)
+    -- G.FUNCS.load_profile creates new profile
 
     APConnect()
 end
@@ -36,13 +49,13 @@ end
 local create_tabsRef = create_tabs
 function create_tabs(args)
     -- when profile interface is created, add archipelago tab 
-    if isInProfileTabCreation then 
+    if isInProfileTabCreation then
         args.tabs[profileAP_Id] = {
-                label = "ARCHIPELAGO",
-                chosen = G.focused_profile == profileAP_Id,
-                tab_definition_function = G.UIDEF.profile_option,
-                tab_definition_function_args = profileAP_Id
-            }
+            label = "ARCHIPELAGO",
+            chosen = G.focused_profile == profileAP_Id,
+            tab_definition_function = G.UIDEF.profile_option,
+            tab_definition_function_args = profileAP_Id
+        }
     end
 
     local create_tabs = create_tabsRef(args)
@@ -50,34 +63,30 @@ function create_tabs(args)
     return create_tabs
 end
 
-
 local profile_selectRef = G.UIDEF.profile_select
 function G.UIDEF.profile_select()
     isInProfileTabCreation = true
-    
+
     local profile_select = profile_selectRef()
 
-    
     isInProfileTabCreation = false
     return profile_select
 end
-
 
 local create_text_inputRef = create_text_input
 function create_text_input(args)
     local create_text_input = create_text_inputRef(args)
 
-    if isInProfileOptionCreation then 
+    if isInProfileOptionCreation then
 
         create_text_input['config']['draw_layer'] = nil
         create_text_input['nodes'][1]['config']['draw_layer'] = nil
 
-
         local ui_letters = create_text_input['nodes'][1]['nodes'][1]['nodes'][1]['nodes'][1]
-        --sendDebugMessage("Is not null: " .. tostring(#ui_letters))
+        -- sendDebugMessage("Is not null: " .. tostring(#ui_letters))
 
-        if #ui_letters > 0 then  
-            ui_letters[#ui_letters]['config']['id'] = 'position_'.. args.prompt_text
+        if #ui_letters > 0 then
+            ui_letters[#ui_letters]['config']['id'] = 'position_' .. args.prompt_text
         end
     end
 
@@ -86,61 +95,104 @@ end
 
 local profile_optionRef = G.UIDEF.profile_option
 function G.UIDEF.profile_option(_profile)
-    G.focused_profile = _profile 
-    if _profile == profileAP_Id then -- AP profile tab code
-        
-        isInProfileOptionCreation = true
-        local t = {n=G.UIT.ROOT, config={align = 'cm', colour = G.C.CLEAR}, nodes={
-        {n=G.UIT.R, config={align = 'cm',padding = 0.1, minh = 0.8}, nodes={
-            ((_profile == G.SETTINGS.profile) or not profile_data) and {n=G.UIT.R, config={align = "cm"}, nodes={
-                create_text_input({
-                    w = 4, max_length = 16, prompt_text = 'Server Address',
-                    -- normally saved values would go into ref table, maybe make json to save?
-                    ref_table = G.AP, ref_value = 'APAddress',extended_corpus = true, keyboard_offset = 1,
-                    callback = function() 
-                      -- code for when enter is hit (?)
-                    end
-                  }),
-                  create_text_input({
-                    w = 4, max_length = 16, prompt_text = 'PORT',
-                    -- normally saved values would go into ref table, maybe make json to save?
-                    ref_table = G.AP, ref_value = 'APPort',extended_corpus = false, keyboard_offset = 1,
-                    callback = function() 
-                      -- code for when enter is hit (?)
-                    end
-                  }),
-                        
-          }}
-        }},
-        {n=G.UIT.R, config={align = 'cm',padding = 0.1, minh = 0.8}, nodes={
-            ((_profile == G.SETTINGS.profile) or not profile_data) and {n=G.UIT.R, config={align = "cm"}, nodes={
-                create_text_input({
-                    w = 4, max_length = 16, prompt_text = 'Slot name',
-                    -- normally saved values would go into ref table, maybe make json to save?
-                    ref_table = G.AP, ref_value = 'APSlot',extended_corpus = true, keyboard_offset = 1,
-                    callback = function() 
-                      -- code for when enter is hit (?)
-                    end
-                  }),
-                  create_text_input({
-                    w = 4, max_length = 16, prompt_text = 'Password',
-                    -- normally saved values would go into ref table, maybe make json to save?
-                    ref_table = G.AP, ref_value = 'APPassword',extended_corpus = true, keyboard_offset = 1,
-                    callback = function() 
-                      -- code for when enter is hit (?)
-                    end
-                  }),
-                        
-          }}
-        }},
-        UIBox_button({button = "APConnect", label = {"Connect"}, minw = 3, Func = G.FUNCS.APConnect})
-      }} 
-      isInProfileOptionCreation = false
-      return t
+    G.focused_profile = _profile
+    if isAPProfileLoaded() then -- AP profile tab code
 
-        
+        isInProfileOptionCreation = true
+        local t = {
+            n = G.UIT.ROOT,
+            config = {
+                align = 'cm',
+                colour = G.C.CLEAR
+            },
+            nodes = {{
+                n = G.UIT.R,
+                config = {
+                    align = 'cm',
+                    padding = 0.1,
+                    minh = 0.8
+                },
+                nodes = {((_profile == G.SETTINGS.profile) or not profile_data) and {
+                    n = G.UIT.R,
+                    config = {
+                        align = "cm"
+                    },
+                    nodes = {create_text_input({
+                        w = 4,
+                        max_length = 16,
+                        prompt_text = 'Server Address',
+                        -- normally saved values would go into ref table, maybe make json to save?
+                        ref_table = G.AP,
+                        ref_value = 'APAddress',
+                        extended_corpus = true,
+                        keyboard_offset = 1,
+                        callback = function()
+                            -- code for when enter is hit (?)
+                        end
+                    }), create_text_input({
+                        w = 4,
+                        max_length = 16,
+                        prompt_text = 'PORT',
+                        -- normally saved values would go into ref table, maybe make json to save?
+                        ref_table = G.AP,
+                        ref_value = 'APPort',
+                        extended_corpus = false,
+                        keyboard_offset = 1,
+                        callback = function()
+                            -- code for when enter is hit (?)
+                        end
+                    })}
+                }}
+            }, {
+                n = G.UIT.R,
+                config = {
+                    align = 'cm',
+                    padding = 0.1,
+                    minh = 0.8
+                },
+                nodes = {((_profile == G.SETTINGS.profile) or not profile_data) and {
+                    n = G.UIT.R,
+                    config = {
+                        align = "cm"
+                    },
+                    nodes = {create_text_input({
+                        w = 4,
+                        max_length = 16,
+                        prompt_text = 'Slot name',
+                        -- normally saved values would go into ref table, maybe make json to save?
+                        ref_table = G.AP,
+                        ref_value = 'APSlot',
+                        extended_corpus = true,
+                        keyboard_offset = 1,
+                        callback = function()
+                            -- code for when enter is hit (?)
+                        end
+                    }), create_text_input({
+                        w = 4,
+                        max_length = 16,
+                        prompt_text = 'Password',
+                        -- normally saved values would go into ref table, maybe make json to save?
+                        ref_table = G.AP,
+                        ref_value = 'APPassword',
+                        extended_corpus = true,
+                        keyboard_offset = 1,
+                        callback = function()
+                            -- code for when enter is hit (?)
+                        end
+                    })}
+                }}
+            }, UIBox_button({
+                button = "APConnect",
+                label = {"Connect"},
+                minw = 3,
+                Func = G.FUNCS.APConnect
+            })}
+        }
+        isInProfileOptionCreation = false
+        return t
+
     else -- if not AP profile behave normally
-        
+
         local profile_option = profile_optionRef(_profile)
         return profile_option
     end
@@ -151,7 +203,7 @@ end
 
 local game_updateRef = Game.update
 function Game.update(arg_298_0, dt)
-    local game_update = game_updateRef(arg_298_0, dt)    
+    local game_update = game_updateRef(arg_298_0, dt)
     if G.APClient ~= nil then
         G.APClient:poll()
     end
@@ -164,16 +216,15 @@ function Game.draw(args)
     local game_draw = game_drawRef(args)
 
     if G.APClient ~= nil and G.APClient:get_state() == AP.State.SLOT_CONNECTED then
-        love.graphics.print("Connected to Archipelago at ".. G.AP.APAddress .. G.AP.APPort.." as ".. G.AP.APSlot, 10, 30)
-        --print("connected")
+        love.graphics.print("Connected to Archipelago at " .. G.AP.APAddress .. G.AP.APPort .. " as " .. G.AP.APSlot,
+            10, 30)
+        -- print("connected")
     else
         love.graphics.print("Not connected to Archipelago.", 10, 30)
     end
 
     return game_draw
 end
-
-
 
 -- load APSettings when opening Profile Select
 -- also create new profile when first loading (might have to move this somewhere more fitting)
@@ -187,14 +238,13 @@ function Game.load_profile(args, _profile)
         sendDebugMessage("Created AP Profile in Slot " .. tostring(profileAP_Id))
     end
 
-    
     local game_load_profile = game_load_profileRef(args, _profile)
 
     local APSettings = load_file('APSettings.json')
 
     APSettings = json.decode(APSettings)
 
-    if APSettings ~= nil  then
+    if APSettings ~= nil then
         G.AP.APSlot = APSettings['APSlot']
         G.AP.APAddress = APSettings['APAddress']
         G.AP.APPort = APSettings['APPort']
@@ -204,7 +254,6 @@ function Game.load_profile(args, _profile)
     return game_load_profile
 end
 
-
 -- other stuff 
 
 -- (not tested)
@@ -212,43 +261,38 @@ end
 
 local check_for_unlockRef = check_for_unlock
 function check_for_unlock(args)
-    
     local check_for_unlock = check_for_unlockRef(args)
 
-    if args.type == 'ante_up' then
-        -- when an ante is beaten
-        if args.ante >= 8 then
-            -- specify the deck
-            if G.deck.name == 'red_deck' then -- (not sure if its actually called 'red_deck' lol)
-                -- specify which stake
-                if G.stake == 1 then
-                    G.APClient:LocationChecks() -- put in corresponding ID
+    if isAPProfileLoaded() then
+
+        if args.type == 'ante_up' then
+            -- when an ante is beaten
+            if args.ante >= 8 then
+                -- specify the deck
+                if G.deck.name == 'red_deck' then -- (not sure if its actually called 'red_deck' lol)
+                    -- specify which stake
+                    if G.stake == 1 then
+                        G.APClient:LocationChecks() -- put in corresponding ID
+                    end
+                    -- else......... (put in rest of checks)
                 end
-                -- else......... (put in rest of checks)
             end
         end
-    end
-
-    -- also need to check for goal completions!
-
     
-    -- if args.type == 'win' then
-        
-    --     --unlock_achievement('heads_up')
-    --     if G.APClient ~= nil and G.APClient:get_state() == AP.State.SLOT_CONNECTED then
-    --         G.APClient:StatusUpdate(AP.ClientStatus.GOAL)
-    --     end
-    -- end
+        -- also need to check for goal completions!
+
+    end
     return check_for_unlock
 end
-
 
 -- fix turning '0' into 'o'
 local zeroWasInput = false
 
 local text_input_keyRef = G.FUNCS.text_input_key
 function G.FUNCS.text_input_key(args)
-    if args.key == '0' then zeroWasInput = true end
+    if args.key == '0' then
+        zeroWasInput = true
+    end
 
     local text_input_key = text_input_keyRef(args)
     zeroWasInput = false
