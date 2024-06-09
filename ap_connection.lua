6b3182efa15ac9ab10218c068a86c39abd846df1
BurndiL <94790464+BurndiL@users.noreply.github.com>
2024-06-09 21:30:59 +0200

    Update ap_connection.lua

--- ap_connection.lua
+++ ap_connection.lua
@@ -3,180 +3,27 @@ G.AP = {
     APPort = 38281,
     APSlot = "Player1",
     APPassword = "",
+    id_offset = 5606000
 }
 
 G.APItems = {
-    [141312111001]={set="Random", order=1},
-    [141312111002]={set="Joker", order=2},
-    [141312111003]={set="Joker", order=3},
-    [141312111004]={set="Joker", order=4},
-    [141312111005]={set="Joker", order=5},
-    [141312111006]={set="Joker", order=6},
-    [141312111007]={set="Joker", order=7},
-    [141312111008]={set="Joker", order=8},
-    [141312111009]={set="Joker", order=9},
-    [141312111010]={set="Joker", order=10},
-    [141312111011]={set="Joker", order=11},
-    [141312111012]={set="Joker", order=12},
-    [141312111013]={set="Joker", order=13},
-    [141312111014]={set="Joker", order=14},
-    [141312111015]={set="Joker", order=15},
-    [141312111016]={set="Joker", order=16},
-    [141312111017]={set="Joker", order=17},
-    [141312111018]={set="Joker", order=18},
-    [141312111019]={set="Joker", order=19},
-    [141312111020]={set="Joker", order=20},
-    [141312111021]={set="Joker", order=21},
-    [141312111022]={set="Joker", order=22},
-    [141312111023]={set="Joker", order=23},
-    [141312111024]={set="Joker", order=24},
-    [141312111025]={set="Joker", order=25},
-    [141312111026]={set="Joker", order=26},
-    [141312111027]={set="Joker", order=27},
-    [141312111028]={set="Joker", order=28},
-    [141312111029]={set="Joker", order=29},
-    [141312111030]={set="Joker", order=30},
-    [141312111031]={set="Joker", order=31},
-    [141312111032]={set="Joker", order=32},
-    [141312111033]={set="Joker", order=33},
-    [141312111034]={set="Joker", order=34},
-    [141312111035]={set="Joker", order=35},
-    [141312111036]={set="Joker", order=36},
-    [141312111037]={set="Joker", order=37},
-    [141312111038]={set="Joker", order=38},
-    [141312111039]={set="Joker", order=39},
-    [141312111040]={set="Joker", order=40},
-    [141312111041]={set="Joker", order=41},
-    [141312111042]={set="Joker", order=42},
-    [141312111043]={set="Joker", order=43},
-    [141312111044]={set="Joker", order=44},
-    [141312111045]={set="Joker", order=45},
-    [141312111046]={set="Joker", order=46},
-    [141312111047]={set="Joker", order=47},
-    [141312111048]={set="Joker", order=48},
-    [141312111049]={set="Joker", order=49},
-    [141312111050]={set="Joker", order=50},
-    [141312111051]={set="Joker", order=51},
-    [141312111052]={set="Joker", order=52},
-    [141312111053]={set="Joker", order=53},
-    [141312111054]={set="Joker", order=54},
-    [141312111055]={set="Joker", order=55},
-    [141312111056]={set="Joker", order=56},
-    [141312111057]={set="Joker", order=57},
-    [141312111058]={set="Joker", order=58},
-    [141312111059]={set="Joker", order=59},
-    [141312111060]={set="Joker", order=60},
-    [141312111061]={set="Joker", order=61},
-    [141312111062]={set="Joker", order=62},
-    [141312111063]={set="Joker", order=63},
-    [141312111064]={set="Joker", order=64},
-    [141312111065]={set="Joker", order=65},
-    [141312111066]={set="Joker", order=66},
-    [141312111067]={set="Joker", order=67},
-    [141312111068]={set="Joker", order=68},
-    [141312111069]={set="Joker", order=69},
-    [141312111070]={set="Joker", order=70},
-    [141312111071]={set="Joker", order=71},
-    [141312111072]={set="Joker", order=72},
-    [141312111073]={set="Joker", order=73},
-    [141312111074]={set="Joker", order=74},
-    [141312111075]={set="Joker", order=75},
-    [141312111076]={set="Joker", order=76},
-    [141312111077]={set="Joker", order=77},
-    [141312111078]={set="Joker", order=78},
-    [141312111079]={set="Joker", order=79},
-    [141312111080]={set="Joker", order=80},
-    [141312111081]={set="Joker", order=81},
-    [141312111082]={set="Joker", order=82},
-    [141312111083]={set="Joker", order=83},
-    [141312111084]={set="Joker", order=84},
-    [141312111085]={set="Joker", order=85},
-    [141312111086]={set="Joker", order=86},
-    [141312111087]={set="Joker", order=87},
-    [141312111088]={set="Joker", order=88},
-    [141312111089]={set="Joker", order=89},
-    [141312111090]={set="Joker", order=90},
-    [141312111091]={set="Joker", order=91},
-    [141312111092]={set="Joker", order=92},
-    [141312111093]={set="Joker", order=93},
-    [141312111094]={set="Joker", order=94},
-    [141312111095]={set="Joker", order=95},
-    [141312111096]={set="Joker", order=96},
-    [141312111097]={set="Joker", order=97},
-    [141312111098]={set="Joker", order=98},
-    [141312111099]={set="Joker", order=99},
-    [141312111100]={set="Joker", order=100},
-    [141312111101]={set="Joker", order=101},
-    [141312111102]={set="Joker", order=102},
-    [141312111103]={set="Joker", order=103},
-    [141312111104]={set="Joker", order=104},
-    [141312111105]={set="Joker", order=105},
-    [141312111106]={set="Joker", order=106},
-    [141312111107]={set="Joker", order=107},
-    [141312111108]={set="Joker", order=108},
-    [141312111109]={set="Joker", order=109},
-    [141312111110]={set="Joker", order=110},
-    [141312111111]={set="Joker", order=111},
-    [141312111112]={set="Joker", order=112},
-    [141312111113]={set="Joker", order=113},
-    [141312111114]={set="Joker", order=114},
-    [141312111115]={set="Joker", order=115},
-    [141312111116]={set="Joker", order=116},
-    [141312111117]={set="Joker", order=117},
-    [141312111118]={set="Joker", order=118},
-    [141312111119]={set="Joker", order=119},
-    [141312111120]={set="Joker", order=120},
-    [141312111121]={set="Joker", order=121},
-    [141312111122]={set="Joker", order=122},
-    [141312111123]={set="Joker", order=123},
-    [141312111124]={set="Joker", order=124},
-    [141312111125]={set="Joker", order=125},
-    [141312111126]={set="Joker", order=126},
-    [141312111127]={set="Joker", order=127},
-    [141312111128]={set="Joker", order=128},
-    [141312111129]={set="Joker", order=129},
-    [141312111130]={set="Joker", order=130},
-    [141312111131]={set="Joker", order=131},
-    [141312111132]={set="Joker", order=132},
-    [141312111133]={set="Joker", order=133},
-    [141312111134]={set="Joker", order=134},
-    [141312111135]={set="Joker", order=135},
-    [141312111136]={set="Joker", order=136},
-    [141312111137]={set="Joker", order=137},
-    [141312111138]={set="Joker", order=138},
-    [141312111139]={set="Joker", order=139},
-    [141312111140]={set="Joker", order=140},
-    [141312111141]={set="Joker", order=141},
-    [141312111142]={set="Joker", order=142},
-    [141312111143]={set="Joker", order=143},
-    [141312111144]={set="Joker", order=144},
-    [141312111145]={set="Joker", order=145},
-    [141312111146]={set="Joker", order=146},
-    [141312111147]={set="Joker", order=147},
-    [141312111148]={set="Joker", order=148},
-    [141312111149]={set="Joker", order=149},
-    [141312111150]={set="Joker", order=150},
-    [141312111151]={set="Tarot", order=2},
-    [141312111152]={set="Tarot", order=3},
-    [141312111153]={set="Tarot", order=4},
-    [141312111154]={set="Tarot", order=5},
-    [141312111155]={set="Tarot", order=6},
-    [141312111156]={set="Tarot", order=7},
-    [141312111157]={set="Tarot", order=8},
-    [141312111158]={set="Tarot", order=9},
-    [141312111159]={set="Tarot", order=10},
-    [141312111160]={set="Tarot", order=11},
-    [141312111161]={set="Tarot", order=12},
-    [141312111162]={set="Tarot", order=13},
-    [141312111163]={set="Tarot", order=14},
-    [141312111164]={set="Tarot", order=15},
-    [141312111165]={set="Tarot", order=16},
-    [141312111166]={set="Tarot", order=17},
-    [141312111167]={set="Tarot", order=18},
-    [141312111168]={set="Tarot", order=19},
-    [141312111169]={set="Tarot", order=20},
-    [141312111170]={set="Tarot", order=21},
-    [141312111171]={set="Tarot", order=22},
+    [G.AP.id_offset + 1]="Red Deck"   ,
+    [G.AP.id_offset + 2]="Blue Deck"  ,
+    [G.AP.id_offset + 3]="Yellow Deck" ,
+    [G.AP.id_offset + 4]="Green Deck" ,
+    [G.AP.id_offset + 5]="Black Deck" ,
+    [G.AP.id_offset + 6]="Magic Deck" ,
+    [G.AP.id_offset + 7]="Nebula Deck",
+    [G.AP.id_offset + 8]="Ghost Deck" ,
+    [G.AP.id_offset + 9]="Abandoned Deck" ,
+    [G.AP.id_offset + 10]="Checkered Deck",
+    [G.AP.id_offset + 11]="Zodiac Deck",
+    [G.AP.id_offset + 12]="Painted Deck",
+    [G.AP.id_offset + 13]="Anaglyph Deck",
+    [G.AP.id_offset + 14]="Plasma Deck" ,
+    [G.AP.id_offset + 15]="Erratic Deck" ,
+    [G.AP.id_offset + 16]="Filler Item" ,
+
 }
 
 G.APSave = {
@@ -207,6 +54,7 @@ function APConnect()
     function on_slot_connected(slot_data)
         print("Slot connected")
         print(slot_data)
+        
         print("missing locations: " .. table.concat(G.APClient.missing_locations, ", "))
         print("checked locations: " .. table.concat(G.APClient.checked_locations, ", "))
         G.APClient:Say("Hello World!")
@@ -223,7 +71,19 @@ function APConnect()
             print("  " .. tostring(player.slot) .. ": " .. player.name ..
                   " playing " .. G.APClient:get_player_game(player.slot))
         end
+        
+        -- set profile name to slot name 
+        G.PROFILES[G.AP.profile_Id]['name'] = G.AP['APSlot']
+        -- just to make sure it's actually loading the right profile
+        G.SETTINGS.profile = G.AP.profile_Id
+        
+        
+        G.FUNCS.load_profile(false)
+        
+        G.FUNCS.set_up_APProfile()
     end
+
+    
 
 
     function on_slot_refused(reasons)
@@ -235,24 +95,20 @@ function APConnect()
         remove_locked = {}
         for _, item in ipairs(items) do
             print(item.item)
-            local item_def = G.APItems[item.item]
-            print(item_def.set)
-            print(item_def.order)
-            print(#G.P_LOCKED)
-            local i=1
-            while i <= #G.P_LOCKED do
-                local card = G.P_LOCKED[i]
-                print(card.set)
-                print(card.order)
-                if card.set == item_def.set and card.order == item_def.order then
-                    unlock_card(card)
-                    table.insert(remove_locked, i)
-                end
-                i = i + 1
-            end
-        end
-        for _, i in pairs(remove_locked) do
-            table.remove(G.P_LOCKED, i)
+            local item_id = item.item
+            local item_name = G.APItems[item.item]
+
+            -- unlock decks by adding their name to the list of unlocked_backs:
+
+            if (item_id - G.AP.id_offset) <= 15 then
+                G.AP.unlocked_backs[item_name] = true
+            end 
+
+            -- local i=1
+            -- while i <= #G.P_LOCKED do
+            --     --leaving this for maybe unlocking cards in future (not planned though)
+            --     i = i+1
+            -- end
         end
     end
 
