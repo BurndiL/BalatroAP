1566d91df481e6d922c3f2e140e00384bd2a3b26
BurndiL <94790464+BurndiL@users.noreply.github.com>
2024-06-10 00:02:01 +0200

    Update randomizer.lua

--- randomizer.lua
+++ randomizer.lua
@@ -281,35 +281,35 @@ function check_for_unlock(args)
             sendDebugMessage("deck_name is " .. deck_name)
             -- specify the deck
             if deck_name == 'Red Deck' then          
-                sendLocationCleared(G.AP.id_offset + (args.ante-1) * 8 + (G.GAME.stake-1))            
+                sendLocationCleared(G.AP.id_offset + (args.ante-2) * 8 + (G.GAME.stake-1))            
             elseif deck_name == 'Blue Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Yellow Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 2 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 2 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Green Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 3 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 3 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Black Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 4 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 4 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Magic Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 5 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 5 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Nebula Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 6 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 6 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Ghost Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 7 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 7 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Abandoned Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 8 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 8 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Checkered Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 9 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 9 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Zodiac Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 10 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 10 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Painted Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 11 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 11 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Anaglyph Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 12 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 12 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Plasma Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 13 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 13 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             elseif deck_name == 'Erratic Deck' then
-                sendLocationCleared(G.AP.id_offset + 64 * 14 + (args.ante-1) * 8 + (G.GAME.stake-1))  
+                sendLocationCleared(G.AP.id_offset + 64 * 14 + (args.ante-2) * 8 + (G.GAME.stake-1))  
             end            
         end
     
